64a21c3e89c008fac5f03893751dafb51fa6bac4
Aledve <jakile971012@gmail.com>
2026-01-20 17:03:05 -0500

    🧹refactor(core): rename main class to VehicleServiceApplication and remove unused objects

--- src/main/java/yps/systems/ai/VehicleServiceApplication.java
+++ src/main/java/yps/systems/ai/VehicleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamServiceApplication {
+public class VehicleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeamServiceApplication.class, args);
+        SpringApplication.run(VehicleServiceApplication.class, args);
     }
 
 }
